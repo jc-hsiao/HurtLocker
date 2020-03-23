ec54194a7889b6e46eb800fefff057c41cbaaa23
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-23 14:15:42 -0400

    fin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,62 +6,43 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    public static int error = 0;
+
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
+    //will give back a String List where the data are separated by ##
+    //naMe:Milk;price:3.23;type:Food;expiration:1/25/2016
+    //naME:BreaD;price:1.23;type:Food;expiration:1/02/2016 ...
+    public static List<String> getProductStrings(String data){
+        List<String> productStrings = new ArrayList<String>();
+        Pattern p = Pattern.compile("(.*?)##");
+        Matcher m = p.matcher(data);
+        while(m.find())
+            productStrings.add(m.group(0));
 
-    public static String replace (String oldS, String newS, String data){
-        Pattern p = Pattern.compile("("+oldS+")");
-        return p.matcher(data).replaceAll(newS);
+        return productStrings;
     }
 
-    public static void main(String[] args) throws Exception{
-        int error = 0;
-
-        String output = (new Main()).readRawDataToString();
-
-        Pattern p = Pattern.compile("(.*?)##");
-        Matcher m = p.matcher(output);
-        List<String> productStrings = new ArrayList<String>();
-        while(m.find()){
-            productStrings.add(replace("##","",m.group()));
-//            System.out.println(replace("##","",m.group()));
-        }
-
-
+    //turn String List into a map
+    public static Map<String, Product> buildMap(List<String> productStrings){
         Map<String, Product> productMap = new LinkedHashMap<>();
 
         for (String str:productStrings) {
-
             Pattern pat = Pattern.compile("(^(?i)name:(.*?);)((?i)price:(.*?);)");
             Matcher mat = pat.matcher(str);
 
-//            Pattern namePattern = Pattern.compile("^(?i)(name:)(.*?);");
-//            Matcher nameMatcher = namePattern.matcher(str);
-//
-//            Pattern pricePattern = Pattern.compile("(?i)(price:)(.*?);");
-//            Matcher priceMatcher = pricePattern.matcher(str);
-
-
-
             while(mat.find()) {
-//                String nameWithoutTag = replace("(?i)(name:)","",mat.group(0));
-//                String name = replace(";","",nameWithoutTag).toLowerCase();
-//                String priceWithoutTag = replace("(?i)(price:)","",mat.group(1));
-//                Double price = Double.valueOf(replace(";","",priceWithoutTag));
-//
-//                System.out.println("NAME: "+mat.group(2));
-//                System.out.println("PRICE: "+mat.group(4));
 
                 String name = "";
-                Double price = 0.0;
+                double price = 0.0;
 
                 try {
-                    name = mat.group(2).toLowerCase();
-                    price = Double.valueOf(mat.group(4));
+                    name = zeroToO(mat.group(2).toLowerCase());
+                    price = Double.parseDouble(mat.group(4));
                     if(name.equals(""))
                         throw new Exception();
                 }catch (Exception e){
@@ -69,17 +50,37 @@ public class Main {
                     continue;
                 }
 
-                if(!productMap.containsKey(name)) {
+                //if the product does not exist in the map, add a new product
+                if(!productMap.containsKey(name))
                     productMap.put(name, new Product(name));
-                }
+
                 productMap.get(name).addPrice(price);
 
             }
         }
-        System.out.println(error);
+        return productMap;
+    }
 
-        for (Map.Entry<String, Product> e :productMap.entrySet()) {
+    //Convert any 0 in the product name to o
+    public static String zeroToO(String name){
+        Pattern p = Pattern.compile("(0)");
+        return p.matcher(name).replaceAll("o");
+    }
+
+
+
+    public static void main(String[] args) throws Exception{
+        //get data
+        String output = (new Main()).readRawDataToString();
+
+        //setup
+        List<String> productStrings = getProductStrings(output);
+        Map<String, Product> productMap = buildMap(productStrings) ;
+
+        //print result
+        for (Map.Entry<String, Product> e :productMap.entrySet())
             System.out.println(e.getValue());
-        }
+
+        System.out.println("Errors         \t \t seen: "+error+" times");
     }
 }
